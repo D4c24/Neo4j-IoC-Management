9d25feb6051ba9829cbc6d73e8b79fed606a11cb
D4c <57877915+D4c24@users.noreply.github.com>
2022-09-14 17:23:59 +0400

    added function upload_data,

--- Project.py
+++ Project.py
@@ -1,5 +1,23 @@
+from neo4j import GraphDatabase
+
+filename = ["Dataset-V4.csv"]
+
+def upload_data():
+  driver=GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "123456"))
+  session=driver.session()
+  query = '''
+  LOAD CSV WITH HEADERS FROM "file:///Dataset-V4.csv" AS line
+  WITH line
+  MERGE (Source:IP {address:line.SrcIP})
+  MERGE (Destination:IP {address:line.DestIP})
+  CREATE (Source)-[:connect_to {
+    `attack`:line.Threat,
+    `dest_port`:line.DestPort}]->(Destination)
+  '''
+  session.run(query).data()
+
 def run():
-  pass
+  upload_data()
 
 if __name__ == '__main__':
   run()
